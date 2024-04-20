b0e9d1afb81edbcbabaa658c1c280419a547afae
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-20 19:13:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ def start(message):
     btn3 = types.KeyboardButton('Контакты')
     markup.add(btn1, btn2, btn3)
     bot.send_message(message.from_user.id, "Общая информация o боте будет в этом сообщении", reply_markup=markup)
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == 'Каталог':
@@ -22,12 +23,15 @@ def get_text_messages(message):
         btn5 = types.KeyboardButton('НАЗАД')
         markup.add(btn1, btn2, btn3, btn4, btn5)
         bot.send_message(message.from_user.id, 'че тебе надо выбирай', reply_markup=markup) 
-        if message.text == 'Steam товары':
-            bot.send_message(message.from_user.id, "ты даун")
     elif message.text == 'ТГ Канал':
         bot.send_message(message.from_user.id, "тут ссылка на канал")
     elif message.text == 'Контакты':
         bot.send_message(message.from_user.id, '@ccody')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == 'Steam товары':
+            bot.send_message(message.from_user.id, "ты даун")
 
 
 
