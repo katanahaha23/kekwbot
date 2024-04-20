99d231283cc5011408c38943aab45050ce2ab4d3
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-20 18:54:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,34 +5,19 @@ bot = telebot.TeleBot('7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Каталог")
+    btn1 = types.KeyboardButton('Каталог')
     markup.add(btn1)
-    
+    bot.send_message(message.from_user.id, "Ты дурак ебаный!", reply_markup=markup)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
     if message.text == 'Каталог':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-        btn1 = types.KeyboardButton('Стим')
-        btn2 = types.KeyboardButton('Стим услуги')
-        btn3 = types.KeyboardButton('тг прем')
-        btn4 = types.KeyboardButton('discord nitro')
-       
-        markup.add(btn1, btn2, btn3, btn4)
-        bot.send_message(message.from_user.id, 'че тебе надо', reply_markup=markup) #ответ бота
-
-
-        if message.text == 'Стим':
-            bot.send_message(message.from_user.id, 'Ле куда преш', parse_mode= 'Markdown')
-        elif message.text == 'Стим услуги':
-            bot.send_message(message.from_user.id, 'сдачу не дам', parse_mode='Markdown')
-        elif message.text == 'тг прем':
-            bot.send_message(message.from_user.id, 'чистка труб звонить 890342342233', parse_mode='Markdown')
-        elif message.text == 'discord nitro':
-            bot.send_message(message.from_user.id, 'ты даун?', parse_mode='Markdown')
-    
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) 
+        btn1 = types.KeyboardButton('пиздапень')
+        btn2 = types.KeyboardButton('хуепень')
+        btn3 = types.KeyboardButton('залупень')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.from_user.id, 'ты хуй', reply_markup=markup) 
 
 
 bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
