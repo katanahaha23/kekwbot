80a9e53b203d55f0a3980b0429d40b1c11237bf7
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-28 00:33:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def get_text_messages(message):
 
 
 @bot.message_handler(content_types=['text'])
-def get_usluges(message):
+def uslugi(message):
     if message.text == 'Steam товары':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton('Вернуться')
