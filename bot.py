86a9af844f11193d1ee071c62f466239da4d0acf
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-27 23:32:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,13 +50,11 @@ def get_text_messages(message):
         markup.add(btn1)
         bot.send_message(message.from_user.id, "ты даун",reply_markup=markup)
     elif message.text == 'НАЗАД':
-        @bot.message_handler(commands=['start'])
-        def start(message):
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton('Каталог')
-            btn2 = types.KeyboardButton('ТГ Канал')
-            btn3 = types.KeyboardButton('Контакты')
-            markup.add(btn1, btn2, btn3)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Каталог')
+        btn2 = types.KeyboardButton('ТГ Канал')
+        btn3 = types.KeyboardButton('Контакты')
+        markup.add(btn1, btn2, btn3)
 
     
 
