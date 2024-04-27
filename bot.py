3b41b2f2f7ef0e0eb4263ece8bc683798004196c
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-28 00:13:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telebot import types
 
 bot = telebot.TeleBot('7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU')
 
-@bot.message_handler(commands=['Меню'])
+@bot.message_handler(commands=['start'])
 def menu(message):
     
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -14,8 +14,3 @@ def menu(message):
     bot.send_message(message.from_user.id, "Общая информация o боте будет в этом сообщении", reply_markup=markup)
 
 
-@bot.message_handler(commands=['Назад'])
-def get_back(message):
-    if start_menu:
-        start_menu = False
-        get_back(message)
