3509ce9f9209ae78351924c841d2c20ca074b3d8
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-28 00:13:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,7 @@ from telebot import types
 bot = telebot.TeleBot('7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU')
 
 @bot.message_handler(commands=['start'])
-def menu(message):
-    
+def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton('Каталог')
     btn2 = types.KeyboardButton('ТГ Канал')
