f678ece4f634aee9ccd4e1d67be6136c57ba2f36
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-29 19:34:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,40 @@
 import telebot
+import sqlite3 as sql
+#База Данных
+con = sql.connect("data.db", check_same_thread=False)
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS users(id INT)")
+
 from telebot import types
 
 bot = telebot.TeleBot('7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU')
 
-channel = "ООФ ‘’FarmCS2 News’’", "-1002072999477", "https://t.me/farmcs2news"
-not_sub = "Подпишись на канал для доступа к этому боту"
+#ID Вашего канала
+chan_id = -1002072999477
 
-async def check_sub_channels(channels, user_id):
-    for channel in channels:
-        chat_member = await bot.get_chat_member(chat_id=channel[1], user_id=user_id)
-        if chat_member['status'] == 'left':
-            return False
-            
-    return True
+#Клавиатура для проверки подписки
+keyboard = telebot.types.InlineKeyboardMarkup()
+subscribe = telebot.types.InlineKeyboardButton(text="Подписаться", url="https://t.me/farmcs2news")
+check = telebot.types.InlineKeyboardButton(text="Проверить", callback_data="check")
+keyboard.add(subscribe)
+keyboard.add(check)
+
+
 
 
 @bot.message_handler(commands=['start'])
-
-
 def start(message):
-    
-    if check_sub_channels(channel, message.from_user.id):
+    users = cur.execute("SELECT id FROM users WHERE id = ?", (message.chat.id, )).fetchone()
+    con.commit()
+    if users == None:
+        bot.send_message(message.from_user.id, "Вы не подписались на канал")
+    else:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton('Каталог')
         btn2 = types.KeyboardButton('ТГ Канал')
         btn3 = types.KeyboardButton('Контакты')
         markup.add(btn1, btn2, btn3)
         bot.send_message(message.from_user.id, "Общая информация o боте будет в этом сообщении", reply_markup=markup)
-    else:
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('ТГ Канал')
-        markup.add(btn1)
-        bot.send_message(message.from_user.id, "Вы не подписались на канал", reply_markup=markup)
 
 
 
