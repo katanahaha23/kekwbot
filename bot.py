30f065232b73a419ebd86f8a3ce51b375a8de0db
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-29 19:22:25 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,34 @@ from telebot import types
 
 bot = telebot.TeleBot('7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU')
 
+channel = "ООФ ‘’FarmCS2 News’’", "-1002072999477", "https://t.me/farmcs2news"
+not_sub = "Подпишись на канал для доступа к этому боту"
+
+async def check_sub_channels(channels, user_id):
+    for channel in channels:
+        chat_member = await bot.get_chat_member(chat_id=channel[1], user_id=user_id)
+        if chat_member['status'] == 'left':
+            return False
+            
+    return True
+
+
 @bot.message_handler(commands=['start'])
+
+
 def start(message):
-    
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton('Каталог')
-    btn2 = types.KeyboardButton('ТГ Канал')
-    btn3 = types.KeyboardButton('Контакты')
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(message.from_user.id, "Общая информация o боте будет в этом сообщении", reply_markup=markup)
+    if check_sub_channels(channel, message.from_user.id):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Каталог')
+        btn2 = types.KeyboardButton('ТГ Канал')
+        btn3 = types.KeyboardButton('Контакты')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.from_user.id, "Общая информация o боте будет в этом сообщении", reply_markup=markup)
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('ТГ Канал')
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, "Вы не подписались на канал", reply_markup=markup)
 
 
 
