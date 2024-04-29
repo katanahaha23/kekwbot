1a2af99e7a2315390311390f8b8f94b619155d4e
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-29 19:48:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,104 +1,23 @@
-import telebot
-import sqlite3 as sql
-#База Данных
-con = sql.connect("data.db", check_same_thread=False)
-cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS users(id INT)")
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
-from telebot import types
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU")
+# Диспетчер
+dp = Dispatcher()
 
-bot = telebot.TeleBot('7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU')
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
 
-#ID Вашего канала
-chan_id = -1002072999477
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
 
-@bot.callback_query_handler(func=lambda call: True)
-def c_listener(call):
-    if call.data == "check":
-        x = bot.get_chat_member(chan_id, call.message.chat.id)
-
-        if x.status == "member" or x.status == "creator" or x.status == "administrator":
-            bot.send_message(call.message.chat.id, "Добро пожаловать!")
-            cur.execute("INSERT INTO users VALUES(?)", (call.message.chat.id, ))
-            con.commit()
-        else:
-            bot.send_message(call.message.chat.id, "Вы не подписались!")
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    users = cur.execute("SELECT id FROM users WHERE id = ?", (message.chat.id, )).fetchone()
-    con.commit()
-    if users == None:
-        bot.send_message(message.from_user.id, "Вы не подписались на канал: https://t.me/farmcs2news")
-    else:
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Каталог')
-        btn2 = types.KeyboardButton('ТГ Канал')
-        btn3 = types.KeyboardButton('Контакты')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.from_user.id, "Общая информация o боте будет в этом сообщении", reply_markup=markup)
-
-
-
-
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == 'Каталог':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) 
-        btn1 = types.KeyboardButton('Товары')
-        btn2 = types.KeyboardButton('Услуги')
-        btn3 = types.KeyboardButton('Telegram Premium')
-        btn4 = types.KeyboardButton('Discord Nitro')
-        btn5 = types.KeyboardButton('НАЗАД')
-        markup.add(btn1, btn2, btn3, btn4, btn5)
-        bot.send_message(message.from_user.id, 'че тебе надо выбирай', reply_markup=markup) 
-    elif message.text == 'ТГ Канал':
-        bot.send_message(message.from_user.id, "тут ссылка на канал")
-    elif message.text == 'Контакты':
-        bot.send_message(message.from_user.id, '@ccody')
-
-    elif message.text == 'Товары':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Вернуться')
-        markup.add(btn1)
-        bot.send_message(message.from_user.id, 'Выберите товар из списка:', reply_markup=markup)
-    elif message.text == 'Услуги':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Вернуться')
-        markup.add(btn1)
-        bot.send_message(message.from_user.id, "Выберите услугу из списка:",reply_markup=markup)
-    elif message.text == 'Telegram Premium':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Вернуться')
-        markup.add(btn1)
-        bot.send_message(message.from_user.id, "Ждите, наш администратор скоро пришлет реквизиты для пополнения. He забудьте прикрепить квитанцию o оплате",reply_markup=markup)
-    elif message.text == 'Discord Nitro':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Вернуться')
-        markup.add(btn1)
-        bot.send_message(message.from_user.id, "ты даун",reply_markup=markup)
-    
-    elif message.text == 'Вернуться':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) 
-        btn1 = types.KeyboardButton('Товары')
-        btn2 = types.KeyboardButton('Услуги')
-        btn3 = types.KeyboardButton('Telegram Premium')
-        btn4 = types.KeyboardButton('Discord Nitro')
-        btn5 = types.KeyboardButton('НАЗАД')
-        markup.add(btn1, btn2, btn3, btn4, btn5)
-        bot.send_message(message.from_user.id, 'че тебе надо выбирай', reply_markup=markup) 
-    
-    elif message.text == 'НАЗАД':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Каталог')
-        btn2 = types.KeyboardButton('ТГ Канал')
-        btn3 = types.KeyboardButton('Контакты')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.from_user.id, "Общая информация o боте будет в этом сообщении", reply_markup=markup)
-
-
-
-
-
-bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
+if __name__ == "__main__":
+    asyncio.run(main())
