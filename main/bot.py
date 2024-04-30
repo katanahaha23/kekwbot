18466190c746dac58bcb9416e00362d14fb6d1bd
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-30 19:48:38 +0700

    Update bot.py

--- main/bot.py
+++ main/bot.py
@@ -1,15 +1,24 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+
 from aiogram.types import Message
 
 bot = Bot("7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU")
 dp = Dispatcher()
 
 
-@dp.message(F.text=="/start")
+@dp.message(Command("start"))
 async def start(message: Message):
-    await message.answer(f"Hello, {message.from_user.first_name}")
+    await message.answer(f"Здарова попуск, {message.from_user.first_name}")
+    
+
+
+@dp.message()
+async def echo(message: Message):
+    await message.reply(f"Че ты высрал?")
+
 
 
 async def main():
