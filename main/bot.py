dc2e202f57c7aedf13856a8a4cbdbecfae9e6f29
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-05-01 00:05:08 +0700

    Update bot.py

--- main/bot.py
+++ main/bot.py
@@ -12,8 +12,9 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: Message):
-    await message.answer(f"Здарова {message.from_user.first_name}", reply_markup=keyboards.main_kb)
     await bot.delete_message(message.chat.id, message.message_id)
+    await message.answer(f"Здарова {message.from_user.first_name}", reply_markup=keyboards.main_kb)
+    
     
 
 
@@ -25,53 +26,63 @@ async def echo(message: Message):
 
 
     if msg == "steam товары":
-        await message.answer(f"Выберите интересующий вас товар: ", reply_markup=keyboards.steam_tovary_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Выберите интересующий вас товар: ", reply_markup=keyboards.steam_tovary_kb)
+        
         
 
 
     elif msg == "steam gifts [5$]":
-        await message.answer(f"Перейдите по ссылке и напишите продавцу:", reply_markup=keyboards.gifts_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Перейдите по ссылке и напишите продавцу:", reply_markup=keyboards.gifts_kb)
+        
 
     elif msg == "аккаунты cs2 prime [13.5$ - 13.9$]":
-        await message.answer(f"Выберите товар:", reply_markup=keyboards.accsprime_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Выберите товар:", reply_markup=keyboards.accsprime_kb)
+        
 
     elif msg == "пустышки":
-        await message.answer(f"Выберите товар:", reply_markup=keyboards.pustyshki_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Выберите товар:", reply_markup=keyboards.pustyshki_kb)
+        
 
 
 
 
     elif msg == "2 ранг, mafiles":
-        await message.answer(f"Вам отправлена ссылка на товар: ", reply_markup=keyboards.gifts_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Вам отправлена ссылка на товар: ", reply_markup=keyboards.gifts_kb)
+       
 
     elif msg == "1 ранг, mafiles":
-        await message.answer(f"Вам отправлена ссылка на товар: ", reply_markup=keyboards.gifts_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Вам отправлена ссылка на товар: ", reply_markup=keyboards.gifts_kb)
+        
 
     elif msg == "пустышки prime cs2, mafiles":
-        await message.answer(f"Вам отправлена ссылка на товар: ", reply_markup=keyboards.gifts_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Вам отправлена ссылка на товар: ", reply_markup=keyboards.gifts_kb)
+       
 
     elif msg == "2 ранг prime cs2, mafiles":
-        await message.answer(f"Вам отправлена ссылка на товар: ", reply_markup=keyboards.gifts_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Вам отправлена ссылка на товар: ", reply_markup=keyboards.gifts_kb)
+       
 
 
 
 
 
     elif msg == "назад":
-        await message.answer(f"Выберите интересующую вас категорию: ", reply_markup=keyboards.cataloge_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Выберите интересующую вас категорию: ", reply_markup=keyboards.cataloge_kb)
+        
     
     elif msg == "вернуться":
-        await message.answer(f"Выберите интересующий вас товар: ", reply_markup=keyboards.steam_tovary_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Выберите интересующий вас товар: ", reply_markup=keyboards.steam_tovary_kb)
+        
     
 
 
@@ -80,14 +91,17 @@ async def echo(message: Message):
 
 
     elif msg == "каталог":
+        await bot.delete_message(message.chat.id, message.message_id)
         await message.answer(f"Выберите интересующую вас категорию: ", reply_markup=keyboards.cataloge_kb)
-        await bot.delete_message(message.chat.id, message.message_id)
+        
     elif msg == "отзывы":
+        await bot.delete_message(message.chat.id, message.message_id)
         await message.answer(f"Отзыв вы можете написать нажав на кнопку под сообщением", reply_markup=keyboards.otzyv_kb)
-        await bot.delete_message(message.chat.id, message.message_id)
+        
     elif msg == "контакты":
-        await message.answer(f"Чтобы связаться с Админом канала нажмите на кнопку ниже", reply_markup=keyboards.contacts_kb)
         await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer(f"Чтобы связаться с Админом канала нажмите на кнопку ниже", reply_markup=keyboards.contacts_kb)
+        
     
 
 
@@ -95,6 +109,7 @@ async def echo(message: Message):
 
 
     else:
+        await bot.delete_message(message.chat.id, message.message_id)
         answers = [
             "че ты высрал? Чтобы начать напиши /start",
             "завали ебало и нажми /start",
@@ -102,7 +117,7 @@ async def echo(message: Message):
             "ну давай высри еще. /start для начала.",
             "может напишешь уже /start ?"
         ]
-        await bot.delete_message(message.chat.id, message.message_id)
+        
         await message.reply(choice(answers))
 
 
