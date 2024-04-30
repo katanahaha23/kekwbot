d3cb5033857ffa33d3302a12e8066e8ed259b908
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-30 21:58:51 +0700

    Update keyboards.py

--- main/keyboards.py
+++ main/keyboards.py
@@ -19,3 +19,21 @@ main_kb = ReplyKeyboardMarkup(
     input_field_placeholder="Выберите одну чушь из спискa:"
 )
 
+cataloge_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Steam Товары"),
+            KeyboardButton(text="Steam Услуги"),
+            KeyboardButton(text="Telegram Premium")
+        ],
+        [
+            KeyboardButton(text="Discord Nitro"),
+            KeyboardButton(text="PornHub Premium")
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder="Выберите одну чушь из спискa:"
+)
+
+
