d4878ba5b497d5d2ce3fb7848a6f9e2b2ae577bf
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-30 19:26:40 +0700

    Update bot.py

--- main/bot.py
+++ main/bot.py
@@ -1,86 +1,22 @@
 import asyncio
-from distutils.dep_util import newer
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
-from aiogram import F
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils.formatting import Text, Bold
-from middlewares.check_sub import CheckSubscription
 
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
 
-# Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
-# Объект бота
-bot = Bot(token="7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU")
-# Диспетчер
+bot = Bot("7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU")
 dp = Dispatcher()
 
-# chan_id = -1002072999477
-dp.message.middleware(CheckSubscription())
-# Обработчик команды /start
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text="Каталог")),
-    builder.add(types.KeyboardButton(text="ТГ канал")),
-    builder.add(types.KeyboardButton(text="Отзывы")),
-    builder.adjust(3)
-    await message.answer("Выберите категорию:",reply_markup=builder.as_markup(resize_keyboard=True),)
+
+@dp.message(F.text=="/start")
+async def start(message: Message):
+    await message.answer(f"Hello, {message.from_user.first_name}")
 
 
-# Ответ на команду Каталог
-@dp.message(F.text.lower() == "каталог")
-async def reply_builder(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text="Steam услуги")),
-    builder.add(types.KeyboardButton(text="Steam товары")),
-    builder.add(types.KeyboardButton(text="Telegram Premium")),
-    builder.add(types.KeyboardButton(text="Discord Nitro")),
-    builder.add(types.KeyboardButton(text="PornHub +")),
-    builder.add(types.KeyboardButton(text="Назад"))
-    builder.adjust(3)
-    await message.answer("Выберите категорию:",reply_markup=builder.as_markup(resize_keyboard=True),)
-
-
-# Ответ на команду ТГ Канал
-@dp.message(F.text.lower() == "тг канал")
-async def tg_chan(message: types.Message):
-    await message.answer("https://t.me/farmcs2news")
-
-
-# Ответ на команду Отзывы
-@dp.message(F.text.lower() == "отзывы")
-async def review(message: types.Message):
-    builder = InlineKeyboardBuilder()
-    builder.row(types.InlineKeyboardButton(
-        text="ОТЗЫВЫ", url="https://github.com")
-    )
-    await message.answer('Нажми на ссылку',reply_markup=builder.as_markup(),)
-
-# Ответ на команду Назад
-@dp.message(F.text.lower() == "назад")
-async def nazad(message: types.Message):
-    kb = [
-        [
-            types.KeyboardButton(text="Каталог"),
-            types.KeyboardButton(text="Тг Канал"),
-            types.KeyboardButton(text="Отзывы")
-        ],
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите..."
-    )
-    await message.answer("Че хотел?", reply_markup=keyboard)
-
-# Запуск процесса поллинга новых апдейтов
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
