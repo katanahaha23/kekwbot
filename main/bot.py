27924b0a2d6c664c25d771794de08967a3c89d66
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-30 20:07:43 +0700

    Update bot.py

--- main/bot.py
+++ main/bot.py
@@ -1,10 +1,10 @@
 import asyncio
 from random import choice
-
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-
 from aiogram.types import Message
+
+import keyboards
 
 bot = Bot("7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU")
 dp = Dispatcher()
@@ -12,14 +12,8 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: Message):
-    await message.answer(f"Здарова попуск, {message.from_user.first_name}")
+    await message.answer(f"Здарова {message.from_user.first_name}", reply_markup=keyboards.main_kb)
     
-
-
-
-
-
-
 
 
 
