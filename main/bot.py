38c73f79f61847a4c6ff2dbf4eea4aa492052877
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-30 23:09:42 +0700

    Update bot.py

--- main/bot.py
+++ main/bot.py
@@ -21,12 +21,16 @@ async def echo(message: Message):
     msg = message.text.lower()
 
     if msg == "каталог":
-        await message.answer(f"Выберите интересующий вас товар: ", reply_markup=keyboards.cataloge_kb)
+        await message.answer(f"Выберите интересующую вас категорию: ", reply_markup=keyboards.cataloge_kb)
     elif msg == "отзывы":
         await message.answer(f"Отзыв вы можете написать нажав на кнопку под сообщением", reply_markup=keyboards.otzyv_kb)
     elif msg == "контакты":
         await message.answer(f"Чтобы связаться с Админом канала нажмите на кнопку ниже", reply_markup=keyboards.contacts_kb)
+    elif msg == "steam товары":
+        await message.answer(f"Выберите интересующий вас товар: ", reply_markup=keyboards.steam_tovary_kb)
 
+
+        
     else:
         answers = [
             "че ты высрал? Чтобы начать напиши /start",
