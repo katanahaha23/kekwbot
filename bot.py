e63b0e15b6b40428778bf7dc88644c506fa7d316
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-30 16:14:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils.formatting import Text, Bold
 
+from aiogram import State, StatesGroup
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -19,70 +20,24 @@ bot = Bot(token="7053088731:AAFgdmKAZ643ZuyYddEIOOGB5ckt9TdEEMU")
 dp = Dispatcher()
 
 # chan_id = -1002072999477
+class SubscriptionState(StatesGroup):
+    waiting_for_subscription = State()
+    waiting_for_confirmation = State()
 
+# Обработчик команды /start
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    # Отправка приветственного сообщения
+    await message.reply("Добро пожаловать! Этот бот предназначен для работы с подпиской на канал")
 
-
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    
-    user_channel_status = await bot.get_chat_member(chat_id='@farmcs2news', user_id=message.from_user.full_name)
-
-    if user_channel_status["status"] != 'left':
-        pass
+    # Проверка подписки на канал
+    channel = 'your_channel_username'
+    subscribed = await bot.get_chat_member(channel, message.from_user.id)
+    if subscribed.status == 'left':
+        await bot.send_message(chat_id=message.chat.id, text='Для работы с ботом необходимо подписаться на наш канал: https://t.me/{}'.format(channel), disable_web_page_preview=True)
+        await SubscriptionState.waiting_for_subscription.set()
     else:
-        await bot.send_message(message.from_user.id, 'text if not in group')
-    
-     
-
-
-
-
-# Ответ на команду Каталог
-@dp.message(F.text.lower() == "каталог")
-async def reply_builder(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text="Steam услуги")),
-    builder.add(types.KeyboardButton(text="Steam товары")),
-    builder.add(types.KeyboardButton(text="Telegram Premium")),
-    builder.add(types.KeyboardButton(text="Discord Nitro")),
-    builder.add(types.KeyboardButton(text="PornHub +")),
-    builder.add(types.KeyboardButton(text="Назад"))
-    builder.adjust(3)
-    await message.answer("Выберите категорию:",reply_markup=builder.as_markup(resize_keyboard=True),)
-
-
-# Ответ на команду ТГ Канал
-@dp.message(F.text.lower() == "тг канал")
-async def tg_chan(message: types.Message):
-    await message.answer("https://t.me/farmcs2news")
-
-
-# Ответ на команду Отзывы
-@dp.message(F.text.lower() == "отзывы")
-async def review(message: types.Message):
-    builder = InlineKeyboardBuilder()
-    builder.row(types.InlineKeyboardButton(
-        text="ОТЗЫВЫ", url="https://github.com")
-    )
-    await message.answer('Нажми на ссылку',reply_markup=builder.as_markup(),)
-
-# Ответ на команду Назад
-@dp.message(F.text.lower() == "назад")
-async def nazad(message: types.Message):
-    kb = [
-        [
-            types.KeyboardButton(text="Каталог"),
-            types.KeyboardButton(text="Тг Канал"),
-            types.KeyboardButton(text="Отзывы")
-        ],
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите..."
-    )
-    await message.answer("Че хотел?", reply_markup=keyboard)
-
+        await bot.send_message(chat_id=message.chat.id, text='Ваш текущий баланс')
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
