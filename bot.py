cb694a7e44c81e3b44aecbcfd9c6f73798cd7348
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-04-30 15:57:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,8 +24,8 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    user_id = message.from_user.id
-    user_channel_status = await bot.get_chat_member(chat_id='@farmcs2news', user_id=user_id)
+    
+    user_channel_status = await bot.get_chat_member(chat_id='@farmcs2news', user_id=message.from_user.id)
 
     if user_channel_status["status"] != 'left':
         pass
