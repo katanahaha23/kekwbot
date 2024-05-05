829161ca67d960683d78197419a82d5c84f7a63c
katanahaha23 <127851952+katanahaha23@users.noreply.github.com>
2024-05-05 12:47:48 +0700

    Create check_sub.py

---
+++ main/check_sub.py
@@ -0,0 +1,25 @@
+from typing import Any, Awaitable, Callable, Dict
+from aiogram import BaseMiddleware
+from aiogram.types import Message, TelegramObject
+
+from keyboards import channel_kb
+
+
+class CheckSubscription(BaseMiddleware):
+
+    async def __call__(
+        self, 
+        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]], 
+        event: Message, 
+        data: Dict[str, Any]
+    ) -> Any:
+        chat_member = await event.bot.get_chat_member("@farmcs2news", event.from_user.id)
+
+        if chat_member.status == "left":
+            await event.answer(
+                "Подписался быстро лох!",
+                reply_markup=channel_kb
+
+            )
+        else:
+            return await handler(event, data)
